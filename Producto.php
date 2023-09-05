ae26adf8e09d33f9795b5a98c914d1b4ad5a2da1
AortizVale <98815707+AortizVale@users.noreply.github.com>
2023-09-05 00:15:12 -0500

    Clase producto creada

---
+++ Producto.php
@@ -0,0 +1,82 @@
+<?php
+
+class Producto {
+    private $id;
+    private $nombre;
+    private $precio;
+    private $imagen;
+
+    public function __construct($id, $nombre, $precio, $imagen) {
+        $this->id = $id;
+        $this->nombre = $nombre;
+        $this->precio = $precio;
+        $this->imagen = $imagen;
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getNombre() {
+        return $this->nombre;
+    }
+
+    public function getPrecio() {
+        return $this->precio;
+    }
+
+    public function getImagen() {
+        return $this->imagen;
+    }
+
+    /**
+     * Set the value of imagen
+     *
+     * @return  self
+     */ 
+    public function setImagen($imagen)
+    {
+        $this->imagen = $imagen;
+
+        return $this;
+    }
+
+    /**
+     * Set the value of precio
+     *
+     * @return  self
+     */ 
+    public function setPrecio($precio)
+    {
+        $this->precio = $precio;
+
+        return $this;
+    }
+
+    /**
+     * Set the value of nombre
+     *
+     * @return  self
+     */ 
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */ 
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+}
+
+
+?>
