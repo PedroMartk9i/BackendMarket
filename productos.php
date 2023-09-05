164e1dd6294138f4ec1c1db30c1a8bfe4acc8808
AortizVale <98815707+AortizVale@users.noreply.github.com>
2023-09-05 00:18:20 -0500

    Update productos.php (Agregar POO)

--- productos.php
+++ productos.php
@@ -13,6 +13,25 @@ session_start();
 </head>
 
 <body>
+<?php
+    require_once 'Producto.php';
+    $producto1= new Producto(1, 'Lays Classic' ,3000, "Imagenes/lays.png"  );
+    $producto2= new Producto(2, 'Colgate Triple Accion' ,6000, "Imagenes/colgate.png"  );
+    $producto3= new Producto(3, 'Arroz Diana 1000g' ,5000, "Imagenes/arroz.png"  );
+    $producto4= new Producto(4, 'Camisa Negra' ,12000, "Imagenes/camisa.png"  );
+    $producto5= new Producto(5, 'Mantequilla Rama' ,16000, "Imagenes/mantequilla.png"  );
+
+
+    $productos = array ( 
+        $producto1,$producto2,$producto3,$producto4,$producto5
+
+       
+
+    );
+
+
+
+    ?>
 
     <div class="titulo-container">
         <h1>Productos Disponibles</h1>
@@ -20,64 +39,33 @@ session_start();
 
 
     <div class="horizontal">
+    
+    <?php
+        
+        foreach ($productos as $value){
+            $imagen=strval ($value->getImagen());
+            $nombre= strval ($value->getNombre());
+            $precio=  ($value->getPrecio());
+            $id= $value->getId();
 
-        <div class="producto">
-            <form action="gestor_carro.php" method="POST">
-                <img src="Imagenes/lays.png" alt="" height="150px" width="150px">
-                <h2>Lays Classic</h2>
-                <p>Precio: 3000 Pesos</p>
-                <button type="submit" name="Agregar_al_carro"><img src="Imagenes/carrito_compra.png" alt="" height="50px" width="50px"></button>
-                <input type="hidden" name="Nombre" value="Lays Classic">
-                <input type="hidden" name="Precio" value="3000">
-                <input type="hidden" name="Imagen" value="Imagenes/lays.png">
+                echo"<div class='producto'>
+            <form action='gestor_carro.php' method='POST'>
+                <img src='$imagen' alt='' height='150px' width='150px'>
+                <h2>$nombre</h2>
+                <p>Precio: $precio Pesos</p>
+                <button type='submit' name='Agregar_al_carro'><img src='Imagenes/carrito_compra.png' alt='' height='50px' width='50px'></button>
+                <input type='hidden' name='Nombre' value='$nombre'>
+                <input type='hidden' name='Precio' value='$precio'>
+                <input type='hidden' name='Imagen' value='$imagen'>
+                <input type='hidden' name='id' value= '$id'>
             </form>
-        </div>
-        <div class="producto">
-            <form action="gestor_carro.php" method="POST">
-                <img src="Imagenes/col_g.jpg" alt="" height="150px" width="150px">
-                <h2>Colgate Triple Accion</h2>
-                <p>Precio: 6000 Pesos</p>
-                <button type="submit" name="Agregar_al_carro"><img src="Imagenes/carrito_compra.png" alt="" height="50px" width="50px"></button>
-                <input type="hidden" name="Nombre" value="Colgate Triple Accion">
-                <input type="hidden" name="Precio" value="6000">
-                <input type="hidden" name="Imagen" value="Imagenes/colgate.png">
-            </form>
-        </div>
-        <div class="producto">
-            <form action="gestor_carro.php" method="POST">
-                <img src="Imagenes/arroz.png" alt="" height="150px" width="150px">
-                <h2>Arroz Diana 1000g</h2>
-                <p>Precio: 5000 Pesos</p>
-                <button type="submit" name="Agregar_al_carro"><img src="Imagenes/carrito_compra.png" alt="" height="50px" width="50px"></button>
-                <input type="hidden" name="Nombre" value="Arroz Diana 1000g">
-                <input type="hidden" name="Precio" value="5000">
-                <input type="hidden" name="Imagen" value="Imagenes/arroz.png">
-            </form>
-        </div>
-        <div class="producto">
-            <form action="gestor_carro.php" method="POST">
-                <img src="Imagenes/camisa.png" alt="" height="150px" width="150px">
-                <h2>Camisa Negra</h2>
-                <p>Precio: 12000 Pesos</p>
-                <button type="submit" name="Agregar_al_carro"><img src="Imagenes/carrito_compra.png" alt="" height="50px" width="50px"></button>
-                <input type="hidden" name="Nombre" value="Camisa Negra">
-                <input type="hidden" name="Precio" value="12000">
-                <input type="hidden" name="Imagen" value="Imagenes/camisa.png">
-            </form>
-        </div>
-        <div class="producto">
-            <form action="gestor_carro.php" method="POST">
-                <img src="Imagenes/mantequilla.png" alt="" height="150px" width="150px">
-                <h2>Mantequilla Rama 500g</h2>
-                <p>Precio: 16000 Pesos</p>
-                <button type="submit" name="Agregar_al_carro"><img src="Imagenes/carrito_compra.png" alt="" height="50px" width="50px"></button>
-                <input type="hidden" name="Nombre" value="Mantequilla Rama 500g">
-                <input type="hidden" name="Precio" value="16000">
-                <input type="hidden" name="Imagen" value="Imagenes/mantequilla.png">
-            </form>
-        </div>
+        </div>";
 
-    </div>
+
+        };
+
+        ?>
+
     <form action="carro.php">
         <button type="submit">
         <h2>Ir al Carrito</h2>
